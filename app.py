30d9607d11a29940d35cc708c94e0819c90a6fbb
singhnavneet77 <143984860+singhnavneet77@users.noreply.github.com>
2024-12-27 00:02:39 +0530

    Add changes to multiple disease prediction app

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import streamlit as st 
 import pickle 
 import os
@@ -10,15 +11,10 @@ st.set_page_config(page_title="Multiple Disease Prediction", layout="wide", page
 working_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Define the paths for your saved models
-diabetes_model_path = os.path.join(working_dir, 'saved_models/diabetes.pkl')
 heart_disease_model_path = os.path.join(working_dir, 'saved_models/heart.pkl')
 kidney_disease_model_path = os.path.join(working_dir, 'saved_models/kindey.pkl')
 
 # Check if the model files exist before attempting to load them
-if os.path.exists(diabetes_model_path):
-    diabetes_model = pickle.load(open(diabetes_model_path, 'rb'))
-else:
-    st.error(f"Diabetes model file not found at {diabetes_model_path}")
 
 if os.path.exists(heart_disease_model_path):
     heart_disease_model = pickle.load(open(heart_disease_model_path, 'rb'))
@@ -30,93 +26,18 @@ if os.path.exists(kidney_disease_model_path):
 else:
     st.error(f"Kidney disease model file not found at {kidney_disease_model_path}")
 
-# Define initial variables
-NewBMI_Overweight = 0
-NewBMI_Underweight = 0
-NewBMI_Obesity_1 = 0
-NewBMI_Obesity_2 = 0 
-NewBMI_Obesity_3 = 0
-NewInsulinScore_Normal = 0 
-NewGlucose_Low = 0
-NewGlucose_Normal = 0 
-NewGlucose_Overweight = 0
-NewGlucose_Secret = 0
 
 # Sidebar menu options
 with st.sidebar:
     selected = option_menu("Multiple Disease Prediction", 
-                           ['Diabetes Prediction',
+                           [
                             'Heart Disease Prediction',
                             'Kidney Disease Prediction'],
                            menu_icon='hospital-fill',
-                           icons=['activity', 'heart', 'person'],
+                           icons=['heart', 'person'],
                            default_index=0)
 
-# Diabetes Prediction
-if selected == 'Diabetes Prediction':
-    st.title("Diabetes Prediction Using Machine Learning")
 
-    col1, col2, col3 = st.columns(3)
-
-    with col1:
-        Pregnancies = st.text_input("Number of Pregnancies")
-    with col2:
-        Glucose = st.text_input("Glucose Level")
-    with col3:
-        BloodPressure = st.text_input("BloodPressure Value")
-    with col1:
-        SkinThickness = st.text_input("SkinThickness Value")
-    with col2:
-        Insulin = st.text_input("Insulin Value")
-    with col3:
-        BMI = st.text_input("BMI Value")
-    with col1:
-        DiabetesPedigreeFunction = st.text_input("DiabetesPedigreeFunction Value")
-    with col2:
-        Age = st.text_input("Age")
-
-    diabetes_result = ""
-    if st.button("Diabetes Test Result"):
-        # Logic for BMI and glucose thresholds
-        if float(BMI) <= 18.5:
-            NewBMI_Underweight = 1
-        elif 18.5 < float(BMI) <= 24.9:
-            pass
-        elif 24.9 < float(BMI) <= 29.9:
-            NewBMI_Overweight = 1
-        elif 29.9 < float(BMI) <= 34.9:
-            NewBMI_Obesity_1 = 1
-        elif 34.9 < float(BMI) <= 39.9:
-            NewBMI_Obesity_2 = 1
-        elif float(BMI) > 39.9:
-            NewBMI_Obesity_3 = 1
-        
-        if 16 <= float(Insulin) <= 166:
-            NewInsulinScore_Normal = 1
-
-        if float(Glucose) <= 70:
-            NewGlucose_Low = 1
-        elif 70 < float(Glucose) <= 99:
-            NewGlucose_Normal = 1
-        elif 99 < float(Glucose) <= 126:
-            NewGlucose_Overweight = 1
-        elif float(Glucose) > 126:
-            NewGlucose_Secret = 1
-
-        user_input = [Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin,
-                      BMI, DiabetesPedigreeFunction, Age, NewBMI_Underweight,
-                      NewBMI_Overweight, NewBMI_Obesity_1, NewBMI_Obesity_2, NewBMI_Obesity_3,
-                      NewInsulinScore_Normal, NewGlucose_Low, NewGlucose_Normal, 
-                      NewGlucose_Overweight, NewGlucose_Secret]
-
-        user_input = [float(x) for x in user_input]
-        prediction = diabetes_model.predict([user_input])
-        if prediction[0] == 1:
-            diabetes_result = "The person has diabetes"
-        else:
-            diabetes_result = "The person does not have diabetes"
-    
-    st.success(diabetes_result)
 
 # Heart Disease Prediction
 if selected == 'Heart Disease Prediction':
